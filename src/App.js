06c5692399678c981262b648a21e3029baf501be
Sarthak Agarwal <sarthak.agarwal@walmart.com>
2024-06-04 11:06:48 +0530

    commit 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BasicGraph from "./Components/BasicGraph";
+
+
+ function App() {
+  return (
+    <div>
+      <BasicGraph/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Components/BasicGraph.js
@@ -0,0 +1,16 @@
+
+import ReactFlow from 'reactflow';
+
+
+const elements = [
+  { id: "1", data: { label: "Parent" }, position: { x: 500, y: 150 } },
+  { id: "2", data: { label: "First child" }, position: { x: 400, y: 250 } },
+  { id: "e1-2", source: "1", target: "2", animated: true }
+];
+
+const graphStyles = { width: "100%", height: "500px" };
+
+
+const BasicGraph = () => <ReactFlow elements={elements} style={graphStyles} />;
+
+export default BasicGraph;
